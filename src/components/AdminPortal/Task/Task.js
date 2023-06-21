46c3806c9842f2d5a60e82aae56acb875eb28392
Karan Sharma <karan.sharma@cginfinity.com>
2023-06-21 21:34:18 +0530

    fixed some ui and errors in assign task and add mentors and modals

--- src/components/AdminPortal/Task/Task.js
+++ src/components/AdminPortal/Task/Task.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import AssignTask from './AssignTask/AssignTask';
-import MentorList from './MentorList/AdminMentorList';
-import "./Task.css"
+import React from "react";
+import AssignTask from "./AssignTask/AssignTask";
+import MentorList from "./MentorList/AdminMentorList";
+import "./Task.css";
 
 const Task = () => {
   return (
     <>
-    
-    <>
-    <div className="assign-task-page">
+      <div className="assign-task-page">
         <div class="container-fluid ">
-            <div class="row mt-4">
-                <div class=" col-md-8 p-0 ">
-                    <AssignTask />
-                </div>
-                <div class=" col-md-4 p-0 skill-added-card" style={{height:"100vh"}}>
-                    <MentorList />
-                </div>
+          <div class="row mt-4">
+            <div class=" col-md-8 p-0 ">
+              <AssignTask />
             </div>
+            <div
+              class=" col-md-4 p-0 skill-added-card"
+              style={{ height: "100vh" }}
+            >
+              <MentorList />
+            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </>
-   
-    </>
-);
-}
+  );
+};
 
-export default Task
+export default Task;
